abfa80d0f3c76314f7afdf2cb036177d53fda316
NoSoyAlonzo <56053393+NoSoyAlonzo@users.noreply.github.com>
2025-05-22 17:01:23 -0700

    cambio ip

--- paginaHt/main.py
+++ paginaHt/main.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
-import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    server_ip = socket.gethostbyname(socket.gethostname())
+    # IP del cliente, considerando si viene desde un proxy inverso
+    client_ip = request.headers.get('X-Real-IP', request.remote_addr)
     server_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return render_template("index.html", server_ip=server_ip, server_time=server_time)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return render_template("index.html", client_ip=client_ip, server_time=server_time)
